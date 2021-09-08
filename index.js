75b3fd48eac475321581171d3ef2594af0afbe2c
Ryan Lazzareschi <57603530+ryanlazz16@users.noreply.github.com>
2021-09-07 17:55:06 -0700

    Duplicate Checking

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ const bg = document.getElementById('bg');
 let emptyEntries = 0;
 let nsEntries = 0;
 let validEntries = 0;
+let numDuplicates = 0;
 
-// chinese simplified, spanish, hindi, arabic, portugese, russian, japanese, javanese, german, korean, french, telugu
+// chinese simplified, spanish, hindi, arabic, portugese, russian, japanese, javanese, german, korean, french, telugu, italian, tagalog
 
 document.getElementById("btn").onclick = generateNFTs;
 
@@ -23,13 +24,15 @@ function generateNFTs() {
                // console.log(results);
                
                var totalWords = 9894
-               var timePerDownload = 600;
+               var timePerDownload = 500;
                var start = 1;
+               // start = 1000;
                var numIterations = totalWords;
+               // numIterations = 25;
 
                var i = start+1;
                var last = i+numIterations;
-               last = totalWords+2;
+               // last = totalWords+2;
                var interval = setInterval(function() { 
                     if (i < last) { 
                          makeNFT(results.data[i], i-1);
@@ -44,7 +47,8 @@ function generateNFTs() {
                     console.log("Empty Entries: " + emptyEntries);
                     console.log("NS Entries: " + nsEntries);
                     console.log("Valid Entries: " + validEntries);
-               }, timePerDownload*(numIterations*1.025));
+                    console.log("Entries with duplicates: " + numDuplicates);
+               }, timePerDownload*(numIterations*1.01));
           }
      });
      // console.log(results);
@@ -55,8 +59,17 @@ function makeNFT(words, num) {
      let numAndName = (("000" + num).slice(-4)) + "_" + nftName;
 
      // update paragraphs with translations
+     var map = {}; 
+     let numDups = 0;
      for (let i = 0; i<elements.length; i++) {
           let word = words[i+1];
+          let lowercaseWord = word.toLowerCase();
+          
+          if (map[lowercaseWord])
+               numDups++;
+          else
+               map[lowercaseWord] = true;
+
           if (word=="") {
                emptyEntries++;
                console.log("Missing entry for " + numAndName);
@@ -71,6 +84,12 @@ function makeNFT(words, num) {
      }
 
      validEntries++;
+
+     if (numDups>=5) {
+          console.log("Lots of duplicates for " + numAndName);
+          numAndName+= "_duplicates";
+          numDuplicates++;
+     }
 
      html2canvas(bg).then(canvas => {
           let downloadLink = document.createElement('a');
