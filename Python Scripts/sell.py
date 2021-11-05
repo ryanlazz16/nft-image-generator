9e82372ae04e63d78d6f09efb2f17a3f21b3b0ad
Ryan Lazzareschi <57603530+ryanlazz16@users.noreply.github.com>
2021-11-05 13:16:01 -0700

    Add automation script

--- Python Scripts/sell.py
+++ Python Scripts/sell.py
@@ -3,13 +3,13 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 
 from selenium.webdriver.support.ui import WebDriverWait
 
 from webdriver_manager.chrome import ChromeDriverManager
 
-TIME_FOR_USER_INTERACTIONS = 3600
+TIME_FOR_USER_INTERACTIONS = 20
+MAX_TIMES_TO_SIGN = 60
 TIME_FOR_PAGE_LOADS = 10
 COLLECTION_NAME = 'digital-dialects'
 NUMBER_NFTs = 7867
@@ -17,70 +17,84 @@ NUMBER_NFTs = 7867
 f = open('number.txt', 'r+')
 START = int(f.read())
 
-s=Service(ChromeDriverManager().install())
+s = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=s)
 
 try:
-	# login user
-	driver.get('https://opensea.io/login?referrer=%2Faccount')
-	xpath='//*[@id="__next"]/div[1]/main/div/div/div/div[2]/ul/li[2]/button'
-	element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
-	element.click()
+    # login user
+    driver.get('https://opensea.io/login?referrer=%2Faccount')
+    xpath='//*[@id="__next"]/div[1]/main/div/div/div/div[2]/ul/li[2]/button'
+    element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
+    element.click()
 
-	# wait for successful login
-	xpath='//*[@id="__next"]/div[1]/main/div/div/div[1]/div[3]/div[2]'
-	element = WebDriverWait(driver, TIME_FOR_USER_INTERACTIONS).until(lambda d: d.find_element(By.XPATH, xpath))
+    # wait for successful login
+    xpath='//*[@id="__next"]/div[1]/main/div/div/div[1]/div[3]/div[2]'
+    element = WebDriverWait(driver, TIME_FOR_USER_INTERACTIONS).until(lambda d: d.find_element(By.XPATH, xpath))
 except:
-	print('Last NFT listed was number', START-1)
-	f.close()
-	quit()
+    print('Last NFT listed was number', START-1)
+    f.close()
+    quit()
 
 url = 'https://opensea.io/collection/'+COLLECTION_NAME+'?search[query]='
 
 # list all NFTS
 for num in range(START, NUMBER_NFTs+1):
-	try:
-		# search for element num
-		numWithZeros = str(num).zfill(4)
-		urlWithQuery = url+numWithZeros
-		driver.get(urlWithQuery)
+    try:
+        # search for element num
+        numWithZeros = str(num).zfill(4)
+        urlWithQuery = url+numWithZeros
 
-		# go to sell link from card
-		xpath='//*[@id="__next"]/div[1]/main/div/div/div[4]/div/div/div/div[3]/div[3]/div[2]/div/div/div/div/article/a'
-		element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
-		link = element.get_attribute("href")
-		sellLink = link+'/sell'
-		driver.get(sellLink)
+        repeat = True
+        numTries = 0
 
-		# get NFT name
-		xpath='//*[@id="__next"]/div[1]/main/div/div/div[1]/div[2]/div/a/div/div[2]/div/div[2]/span[2]'
-		element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
-		nftName = element.text
+        while repeat:
+            try:
+                numTries += 1
 
-		# click price box
-		xpath='//*[@id="__next"]/div[1]/main/div/div/div[3]/div/div[2]/div/div[1]/form/div[1]/div/div[2]/div/div/div[2]/input'
-		element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
+                # go to sell link from card
+                driver.get(urlWithQuery)
+                xpath='//*[@id="__next"]/div[1]/main/div/div/div[4]/div/div/div/div[3]/div[3]/div[2]/div/div/div/div/article/a'
+                element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
+                link = element.get_attribute("href")
+                sellLink = link+'/sell'
+                driver.get(sellLink)
 
-		# type price and click complete listing
-		element.send_keys('.01')
-		xpath='//*[@id="__next"]/div[1]/main/div/div/div[3]/div/div[2]/div/div[1]/form/div[5]/button'
-		element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
-		element.click()
+                # get NFT name
+                xpath='//*[@id="__next"]/div[1]/main/div/div/div[1]/div[2]/div/a/div/div[2]/div/div[2]/span[2]'
+                element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
+                nftName = element.text
 
-		# click sign button
-		xpath='/html/body/div[4]/div/div/div/section/div/div/section/div/div/div/div/div/div/div/button'
-		element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
-		element.click()
+                # click price box
+                xpath='//*[@id="__next"]/div[1]/main/div/div/div[3]/div/div[2]/div/div[1]/form/div[1]/div/div[2]/div/div/div[2]/input'
+                element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
 
-		# wait til user signs 
-		xpath='/html/body/div[4]/div/div/div/div[1]/section/p/a'
-		element = WebDriverWait(driver, TIME_FOR_USER_INTERACTIONS).until(lambda d: d.find_element(By.XPATH, xpath))
+                # type price and click complete listing
+                element.send_keys('.01')
+                xpath='//*[@id="__next"]/div[1]/main/div/div/div[3]/div/div[2]/div/div[1]/form/div[5]/button'
+                element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
+                element.click()
 
-		print('Just listed '+nftName+'! See it here:', urlWithQuery)
-	except:
-		print('Last NFT listed was number', num-1)
-		f.seek(0)
-		f.write(str(num))
-		f.truncate()
-		f.close()
-		quit()
+                # click sign button
+                xpath='/html/body/div[4]/div/div/div/section/div/div/section/div/div/div/div/div/div/div/button'
+                element = WebDriverWait(driver, TIME_FOR_PAGE_LOADS).until(lambda d: d.find_element(By.XPATH, xpath))
+                element.click()
+
+                # wait til user signs 
+                xpath='/html/body/div[4]/div/div/div/div[1]/section/p/a'
+                element = WebDriverWait(driver, TIME_FOR_USER_INTERACTIONS).until(lambda d: d.find_element(By.XPATH, xpath))
+
+                repeat = False
+            except:
+                print('User did not sign. Trying again.')
+                if numTries == MAX_TIMES_TO_SIGN:
+                    print('User failed to sign in the given time.')
+                    raise RecursionError
+
+        print('Just listed '+nftName+'! See it here:', urlWithQuery)
+    except:
+        print('Last NFT listed was number', num-1)
+        f.seek(0)
+        f.write(str(num))
+        f.truncate()
+        f.close()
+        quit()
--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 
 An example of the NFTs can be seen at either of the links above
 
-index.js is a Node.js script that creates that creates art pieces (NFTs) from a dataset of words with translations. The script works by displaying words over a black background on a webpage and "screenshotting" the webpage, then repeating for all words and translations. Similar scripts were used to create the artwork for the collection, and can be found in "Banner Image" and "Featured Image" folders. 
+index.js is a Node.js script that creates that creates art pieces (NFTs) from a dataset of words with translations. The script works by displaying words over a black background on a webpage and "screenshotting" the webpage, then repeating for all words and translations. Similar scripts were used to create the artwork for the collection, and can be found in "Banner Image" and "Featured Image" folders.
 
 The "Python Scripts" folder contains various python scripts that rename NFT files so that they're organized, orderly, and ready for deployment to the OpenSea marketplace
+
+"Python Scripts/sell.py" is an automation script built with Selenium that automatically lists NFTs to OpenSea
